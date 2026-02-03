e1cfc10fbea0cb57a1f33b67f4cc836c1e76adbb
Felipe Stanzani <stanzani@gmail.com>
2026-02-03 18:02:45 -0300

    First commit

---
+++ src/main/java/com/felipestanzani/migrationdemo/MigrationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.felipestanzani.migrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationdemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MigrationdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipestanzani/migrationdemo/MigrationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipestanzani.migrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
